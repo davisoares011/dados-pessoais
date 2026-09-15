f3b4792560dd7a537747f1656534d207e8e4808d
DAVI SOARES ARAUJO <davi.saraujo5@senacsp.edu.br>
2026-09-15 20:35:49 -0300

    Etapa 01

---
+++ src/test/java/br/senac/tads/dsw/dados_pessoais/DadosPessoaisApplicationTests.java
@@ -0,0 +1,14 @@
+// hash-identificacao: 86a46359b1d20393d6905f1c15cb353eb2c3c8c8f03799f4bfd1df812be0bf4d
+package br.senac.tads.dsw.dados_pessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
